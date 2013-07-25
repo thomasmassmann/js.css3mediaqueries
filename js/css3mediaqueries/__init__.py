809df8c7decfdc8898840d434c876031993db820
Thomas Massmann <thomas@propertyshelf.com>
2013-07-25 13:01:31 +0200

    Updated renderer.

--- js/css3mediaqueries/__init__.py
+++ js/css3mediaqueries/__init__.py
@@ -1,17 +1,16 @@
 from fanstatic import Library, Resource
+import fanstatic
 
 library = Library('css3mediaqueries', 'resources')
 
 
-def renderer(url):
-    return """
-<!--[if lt IE 9 ]>
-  <script src="%s"></script>
-<![endif]-->""" % url
+def lte_ie8_renderer(url):
+    inner = fanstatic.core.render_js(url)
+    return "<!--[if lte IE 8 ]>%s<![endif]-->" % inner
 
 
 css3mediaqueries = Resource(
     library, 'css3-mediaqueries.js',
     bottom=True,
-    renderer=renderer,
+    renderer=lte_ie8_renderer,
 )
